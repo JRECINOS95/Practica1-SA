206c9d69703b5548445aeee770442a8418a77b61
fredyramirez <fredyramirezsando@gmail.com>
2021-06-18 17:32:58 -0600

    Update encrip.ts

--- apiUsuarios/src/utils/encrip.ts
+++ apiUsuarios/src/utils/encrip.ts
@@ -14,6 +14,7 @@ export function encryptUsingAES256(value:string) {
     return encrypted.toString();
 }
 
+
 export function decryptUsingAES256(decString:string) {
     var decrypted = CryptoJS.AES.decrypt(decString, key, {
         keySize: 128 / 8,
